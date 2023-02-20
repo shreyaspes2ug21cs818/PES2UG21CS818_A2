7a3094696a1c0a221bb0f5c9c92510d564c50e08
SHREYAS <shreyaskannar2001@gmail.com>
2023-02-20 22:01:59 +0530

    committing this document

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp from PES2UG21CS818
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
